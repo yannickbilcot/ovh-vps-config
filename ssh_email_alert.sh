10bbdec4d27e5955b52c2c9c26e7fd6c1f3e8f19
Yannick BILCOT <yannick.bilcot@laposte.net>
2021-05-14 14:48:51 +0800

    update email css

--- ssh_email_alert.sh
+++ ssh_email_alert.sh
@@ -5,10 +5,10 @@ SUBJECT="[$(hostname)] SSH notification: successful login from IP ${PAM_RHOST}"
 BODY="$(cat <<EOF
 <html>
 <body style="font: small/ 1.5 Arial,Helvetica,sans-serif;padding: 15px;">
-  <h1 style="font: small/ 1.5 Arial,Helvetica,sans-serif;">SSH Login successful</h1>
+  <h1 style="font: small/ 1.5 Arial,Helvetica,sans-serif normal;">SSH Login successful</h1>
   <p>The following user signed to your server via SSH, please review the information below:</p>
   <p>
-    <table style="font-size: 13px;">
+    <table style="font-size: small;">
       <tr><td class="key" style="color: #76808f;text-align: right;padding-right: 5px;">User:</td><td>${PAM_USER}</td></tr>
       <tr><td class="key" style="color: #76808f;text-align: right;padding-right: 5px;">IP Address:</td><td>${PAM_RHOST}</td></tr>
       <tr><td class="key" style="color: #76808f;text-align: right;padding-right: 5px;">Service:</td><td>${PAM_SERVICE}</td></tr>
@@ -16,7 +16,7 @@ BODY="$(cat <<EOF
     </table>
   </p>
   <p>
-    <h3 style="font: small/ 1.5 Arial,Helvetica,sans-serif;">Whois information</h3>
+    <h3 style="font: small/ 1.5 Arial,Helvetica,sans-serif normal;">Whois information</h3>
     <div class="whois" style="display: inline-block;font-family: monospace;line-height: 1.3em;word-spacing: .01em;word-wrap: break-word;padding: 15px;background-color: #f5f5f5;border: 1px solid #e3e3e3;box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.05);">
 $(whois ${PAM_RHOST} | awk '{print $0"<br>"}')
     </div>
